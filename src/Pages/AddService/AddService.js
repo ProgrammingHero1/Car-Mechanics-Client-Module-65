e69c557833931a21453bfe33f84830c7cec347f2
Programming Hero <programming.hero1@gmail.com>
2021-10-21 22:50:05 -0500

    added service form

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,33 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css';
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+
+        axios.post('http://localhost:5000/services', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert('added successfully');
+
+                }
+            })
+    }
+
     return (
-        <div>
-            <h2>Add a Service</h2>
+        <div className="add-service">
+            <h2>Please Add a Service</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
+                <textarea {...register("description")} placeholder="Description" />
+                <input type="number" {...register("price")} placeholder="price" />
+                <input {...register("img")} placeholder="image url" />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
