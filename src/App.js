211cef9301417bfd5c34d76e9930ff889e19e470
Programming Hero <programming.hero1@gmail.com>
2021-10-21 21:40:04 -0500

    added add service route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
+import AddService from './Pages/AddService/AddService';
 
 
 function App() {
@@ -28,6 +29,9 @@ function App() {
             <PrivateRoute path="/booking/:serviceId">
               <Booking></Booking>
             </PrivateRoute>
+            <Route path="/addService">
+              <AddService></AddService>
+            </Route>
             <Route path="*">
               <NotFound></NotFound>
             </Route>
---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddService = () => {
+    return (
+        <div>
+            <h2>Add a Service</h2>
+        </div>
+    );
+};
+
+export default AddService;
